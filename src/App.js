6262fdbc61e28a1b1b2441bbbd4ca9b1a04f06f3
Devashish Patel (Dev) <dev.patel8787@gmail.com>
2021-05-02 12:12:17 -0400

    pricing cards

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import Quiz from "./Components/Quiz";
 // import Quiz from "./Components/QuizSep";
+import PricingCards from "./Components/PricingCards/PricingCards";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
       <h1>MultiSteps Helper Function</h1>
+      <PricingCards />
       <Quiz
         easyStartUrl="https://quickbooks.intuit.com/gutenberg/ca/pricing-easystart/"
         plusUrl="https://quickbooks.intuit.com/gutenberg/ca/pricing-plus/"
         essentialsUrl="https://quickbooks.intuit.com/gutenberg/ca/pricing-essentials/"
         selfemployedUrl="https://quickbooks.intuit.com/gutenberg/ca/pricing-self-employed/"
       />
-      <p>test</p>
     </div>
   );
 }
---
+++ src/Components/PricingCards/PricingCards.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./PricingCards.css";
+
+export default function PricingCards() {
+  return (
+    <div className="pricingCards">
+      <div className="pricingCardSingle">pricingCardSingle 1</div>
+      <div className="pricingCardSingle"> pricingCardSingle 2</div>
+      <div className="pricingCardSingle">pricingCardSingle 3</div>
+    </div>
+  );
+}
